fcd00a86690c203176332a4cfd608571eab72701
Sumit Mukharjee <sumitmukharjeeeeee@gmail.com>
2024-01-25 14:10:51 +0530

    Adding palindromic String

--- Basic/src/palindromicString.java
+++ Basic/src/palindromicString.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class palindromicString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-
-
+        String s = sc.nextLine();
+        String s2 = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            s2 += s.charAt(i);
+        }
+        if (s2.equals(s)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("not");
+        }
     }
 }
