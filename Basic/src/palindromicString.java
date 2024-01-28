179ca421645a9ee9c9846ba34678c88777d5fe36
Sumit Mukharjee <sumitmukharjeeeeee@gmail.com>
2024-01-28 12:04:34 +0530

    Using Stringbuilder

--- Basic/src/palindromicString.java
+++ Basic/src/palindromicString.java
@@ -4,11 +4,11 @@ public class palindromicString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String s2 = "";
+        StringBuilder s2 = new StringBuilder();
         for (int i = s.length() - 1; i >= 0; i--) {
-            s2 += s.charAt(i);
+            s2.append(s.charAt(i));
         }
-        if (s2.equals(s)) {
+        if (s2.toString().equals(s)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("not");
